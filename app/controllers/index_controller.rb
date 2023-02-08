8c294a5884a2f2b58d4d3487b8077695760accf9
Vais Mahendran <vais.mahendran@gmail.com>
2023-02-07 21:05:49 -0500

    add ability to lookup word

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,13 @@
 class IndexController < ApplicationController
     def index
+        response = HTTParty.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{params[:search]}")
+
+        binding.pry
+
+        if response.code == 200
+            @word = response.first
+        else
+            @word = nil
+        end
     end
 end
